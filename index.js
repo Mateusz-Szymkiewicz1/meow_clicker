ec0aafe4479cb5037760b9f7e5b63223740583f4
unknown <szymkiewiczmateusz1@gmail.com>
2022-12-16 16:00:50 +0100

    Skin system

--- index.js
+++ index.js
@@ -2,6 +2,8 @@ let cps = 0;
 let score = 0;
 window.click_strength = 1;
 window.idle_clicks = 0;
+window.skins = ["robert"];
+window.current_skin = "robert";
 document.querySelector("#label_strength").innerText = `Stronger click (${window.click_strength})`;
 document.querySelector("#label_idle").innerText = `Auto clicking (${window.idle_clicks})`;
 function randomNumber(min, max) {
@@ -9,11 +11,51 @@ function randomNumber(min, max) {
     max = Math.floor(max);
     return Math.floor(Math.random() * (max - min + 1)) + min;
 }
+if(window.localStorage.getItem("score")){
+            let json = JSON.parse(window.localStorage.getItem("score"));
+            score = parseInt(json.score);
+            window.click_strength = parseInt(json.click_strength);
+            document.querySelector("#label_strength").innerText = `Stronger click (${window.click_strength})`;
+            document.querySelector("#buy_strength").innerText = `Buy (${window.click_strength*100})`;
+            document.querySelector("#buy_strength").dataset.price = window.click_strength*100;
+            window.idle_clicks = parseInt(json.idle_clicks);
+            document.querySelector("#label_idle").innerText = `Stronger click (${window.idle_clicks})`;
+            document.querySelector("#buy_idle").innerText = `Buy (${(window.idle_clicks+1)*200})`;
+            document.querySelector("#buy_idle").dataset.price = (window.idle_clicks+1)*200;
+            document.querySelector("span").innerText = `MeowCount: ${score}`;
+            window.skins = json.skins;
+            window.current_skin = json.current_skin;
+}
+document.querySelector("#cat_img").src = `images/${window.current_skin}.jpg`;
+skiny_check();
+function skiny_check(){
+    document.querySelectorAll(".skin").forEach(el => {
+        window.skins.forEach(e => {
+          if(el.dataset.name == e){
+              el.querySelector(".skin_buy").setAttribute("style", "pointer-events: none;filter: contrast(0.5);");
+              el.querySelector(".skin_buy").innerText = "Posiadasz";
+              el.dataset.state = 1;
+              if(el.dataset.name == window.current_skin){
+                el.querySelector(".skin_set").setAttribute("style", "pointer-events: none;filter: contrast(0.5);");
+                el.querySelector(".skin_set").innerText = "Ustawiony";
+            }else{
+                el.querySelector(".skin_set").setAttribute("style", "pointer-events: auto;filter: contrast(1);");
+                el.querySelector(".skin_set").innerText = "Ustaw";
+            }
+          }
+        })
+        if(el.dataset.state == 0){
+            el.querySelector(".skin_set").setAttribute("style", "pointer-events: none;filter: contrast(0.5);");
+        }
+    })
+}
 function save(){
   window.localStorage.setItem("score", JSON.stringify({
     score: score,
     click_strength: window.click_strength,
     idle_clicks: window.idle_clicks,
+    skins: window.skins,
+      current_skin: window.current_skin,
   }));
 }
 function click_effect(e){
@@ -41,23 +83,10 @@ async function decision(){
     })
   })
 }
-        if(window.localStorage.getItem("score")){
-            let json = JSON.parse(window.localStorage.getItem("score"));
-            score = parseInt(json.score);
-            window.click_strength = parseInt(json.click_strength);
-            document.querySelector("#label_strength").innerText = `Stronger click (${window.click_strength})`;
-            document.querySelector("#buy_strength").innerText = `Buy (${window.click_strength*100})`;
-            document.querySelector("#buy_strength").dataset.price = window.click_strength*100;
-            window.idle_clicks = parseInt(json.idle_clicks);
-            document.querySelector("#label_idle").innerText = `Stronger click (${window.idle_clicks})`;
-            document.querySelector("#buy_idle").innerText = `Buy (${(window.idle_clicks+1)*200})`;
-            document.querySelector("#buy_idle").dataset.price = (window.idle_clicks+1)*200;
-            document.querySelector("span").innerText = `MeowCount: ${score}`;
-        }
         document.querySelector("#cat_img").addEventListener("click", function(e){
             cps++;
             score = parseInt(score+window.click_strength);
-            if(score == 2137){
+            if(score == 100000){
                document.querySelector("canvas").style.display = "block"; 
                document.querySelector("#cat_img").style.animation = "spin 2s infinite"; 
                document.querySelector(".score").style.animation = "spin 2s infinite"; 
@@ -74,13 +103,14 @@ async function decision(){
             audio.play();
             document.querySelector(".score").innerText = `MeowCount: ${score}`;
             document.querySelector(".shop > h3").innerText = score+"C";
+            document.querySelector(".skiny > h3").innerText = score+"C";
             save();
         })
         document.body.addEventListener("keyup", function(e){
             cps++;
             if(e.code == "Space"){
             score = parseInt(score+window.click_strength);
-            if(score == 2137){
+            if(score == 100000){
                document.querySelector("canvas").style.display = "block"; 
                document.querySelector("#cat_img").style.animation = "spin 2s infinite"; 
                document.querySelector(".score").style.animation = "spin 2s infinite"; 
@@ -96,6 +126,7 @@ async function decision(){
             audio.play();
             document.querySelector(".score").innerText = `MeowCount: ${score}`;
             document.querySelector(".shop > h3").innerText = score+"C";
+            document.querySelector(".skiny > h3").innerText = score+"C";
             save();
         }
         })
@@ -127,7 +158,8 @@ async function decision(){
            save();
             document.querySelector(".score").innerText = `MeowCount: ${score}`;
             document.querySelector(".shop > h3").innerText = score+"C";
-            if(score == 2137){
+            document.querySelector(".skiny > h3").innerText = score+"C";
+            if(score == 100000){
               document.querySelector("canvas").style.display = "block"; 
               document.querySelector("#cat_img").style.animation = "spin 2s infinite"; 
               document.querySelector(".score").style.animation = "spin 2s infinite"; 
@@ -146,6 +178,18 @@ document.body.addEventListener("click", function(e){
             document.querySelector(".shop").style.animation = "slideOutRight 0.5s ease"
             setTimeout(function(){
                 document.querySelector(".shop").style.display = "none";
+            }, 500) 
+        }
+    }
+      if(e.target.className == "x_icon_skins"){
+        if(e.target.id == "span_skins"){
+            document.querySelector(".skiny").style.animation = "slideInRight 0.5s ease"
+            document.querySelector(".skiny").style.display = "block";
+            document.querySelector(".skiny > h3").innerText = score+"C";
+        }else{
+            document.querySelector(".skiny").style.animation = "slideOutRight 0.5s ease"
+            setTimeout(function(){
+                document.querySelector(".skiny").style.display = "none";
             }, 500) 
         }
     }
@@ -174,7 +218,8 @@ document.body.addEventListener("click", function(e){
             save();
             document.querySelector(".score").innerText = `MeowCount: ${score}`;
             document.querySelector(".shop > h3").innerText = score+"C";
-            if(score == 2137){
+            document.querySelector(".skiny > h3").innerText = score+"C";
+            if(score == 100000){
               document.querySelector("canvas").style.display = "block"; 
               document.querySelector("#cat_img").style.animation = "spin 2s infinite"; 
               document.querySelector(".score").style.animation = "spin 2s infinite"; 
@@ -200,6 +245,35 @@ document.body.addEventListener("click", function(e){
               }, 1000)
             }
         }
+        e.target.blur();
+    }
+    if(e.target.className == "skin_buy"){
+        if(score >= e.target.dataset.price){
+            window.skins.push(e.target.parentElement.dataset.name);
+            score -= e.target.dataset.price;
+            skiny_check();
+            save();
+        }else{
+           if(!document.querySelector(".span_bieda")){
+            document.querySelector(".skiny > h3").innerHTML =  document.querySelector(".skiny > h3").innerHTML+`<br /><span class="span_bieda">Nie stać cie ;(</span>`;
+            document.querySelector(".skiny > h3").style.animation = "shake 1s ease";
+            setTimeout(function(){
+              document.querySelector(".skiny > h3").style.animation = "none";
+            }, 1000)
+            }else{
+              document.querySelector(".skiny > h3").style.animation = "shake 1s ease";
+              setTimeout(function(){
+                document.querySelector(".skiny > h3").style.animation = "none";
+              }, 1000)
+            } 
+        }
+        e.target.blur();
+    }
+    if(e.target.className == "skin_set"){
+        window.current_skin = e.target.parentElement.dataset.name;
+        document.querySelector("#cat_img").src = `images/${e.target.parentElement.dataset.name}.jpg`;
+        save();
+        skiny_check();
         e.target.blur();
     }
 })
